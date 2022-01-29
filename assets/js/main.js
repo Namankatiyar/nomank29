9e64356d5c41e054280c39f56134fb8882bfb312
Naman Katiyar <76195248+Namankatiyar@users.noreply.github.com>
2022-01-29 11:59:47 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,26 +35,26 @@ const sr = ScrollReveal({
 
 /*SCROLL HOME*/
 sr.reveal('.home__title',{}); 
-sr.reveal('.button',{delay: 200}); 
-sr.reveal('.home__img',{delay: 400}); 
-sr.reveal('.home__social-icon',{ interval: 200}); 
+sr.reveal('.button',{delay: 50}); 
+sr.reveal('.home__img',{delay: 50}); 
+sr.reveal('.home__social-icon',{ interval: 50}); 
 
 /*SCROLL ABOUT*/
 sr.reveal('.about__img',{}); 
-sr.reveal('.about__subtitle',{delay: 400}); 
-sr.reveal('.about__text',{delay: 400}); 
+sr.reveal('.about__subtitle',{delay: 50}); 
+sr.reveal('.about__text',{delay: 50}); 
 
 /*SCROLL SKILLS*/
 sr.reveal('.skills__subtitle',{}); 
 sr.reveal('.skills__text',{}); 
-sr.reveal('.skills__data',{interval: 200}); 
-sr.reveal('.skills__img',{delay: 600});
+sr.reveal('.skills__data',{interval: 50}); 
+sr.reveal('.skills__img',{delay: 50});
 
 /*SCROLL WORK*/
-sr.reveal('.work__img',{interval: 200}); 
+sr.reveal('.work__img',{interval: 50}); 
 
 /*SCROLL CONTACT*/
-sr.reveal('.contact__input',{interval: 200}); 
+sr.reveal('.contact__input',{interval: 50}); 
 
 /* Dark Mode */
 function darkmode(){
